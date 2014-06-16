3a0152327717ecc3f67c2f1108d2ad16d2c3872a
Danny Bradbury <danny@itjournalist.com>
2014-06-16 15:58:49 -0700

    Added gravatar functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  require 'digest/md5'
+  before_save :build_gravatar
   # see: https://github.com/plataformatec/devise
   devise :database_authenticatable, :registerable, :lockable,
          :recoverable, :rememberable, :trackable, :validatable
@@ -8,9 +10,11 @@ class User < ActiveRecord::Base
 
   validates :name, :presence => true
   validates :username, :presence => true, :uniqueness => true
+  validates :email, :presence => true, :uniqueness => true
 
   # Scope method to get all users except the one passed.
   #
+
   def self.all_except(user)
     User.where(arel_table[:id].not_eq(user.id)).order(:name)
   end
@@ -21,4 +25,9 @@ class User < ActiveRecord::Base
   def all_twets
     Twet.by_user_ids(id, *follows.map(&:following_id))
   end
+
+  def build_gravatar
+    self.gravatar = "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(self.email.downcase)}"
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+w = User.create!(name: "John Appleseed", username: "john", email: "john@ibm.com", password: "12341234")
+w = User.create!(name: "Cindy Pindy", username: "cindy", email: "cindy@oracle.com", password: "12341234")
+w = User.create!(name: "Steve Jobs", username: "steve", email: "steve@apple.com", password: "12341234")
+w = User.create!(name: "Mike Stipes", username: "striper", email: "mike@sun.com", password: "12341234")
+w = User.create!(name: "Mike Paperclip", username: "microsoftbob", email: "bob@microsoft.com", password: "12341234")
+
+for f in 1..75
+	user_id = 1 + rand(4)
+	content = Faker::Company.catch_phrase 
+	w = Twet.create!(user_id: user_id, content: content)
+end
